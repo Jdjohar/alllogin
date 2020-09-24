07cc8fc64dbfd0ab8361e1cf4c4f6a0f2d376754
Jashandeep Singh <jdeep514@gmail.com>
2020-09-24 16:29:19 +0530

    fb login

--- auth/facebook.js
+++ auth/facebook.js
@@ -3,8 +3,8 @@ var passport = require('passport')
 var User = require('../models/User');
 
 passport.use(new FacebookStrategy({
-    clientID: "159030901322260",
-    clientSecret: "0d641e47f5d55af221ec80346f3f2d43",
+    clientID: "610954016431255",
+    clientSecret: "d9dda63d6d79bd0171f8b9b73c59329d",
     callbackURL: "http://127.0.0.1:3000/auth/facebook/callback"
   },
   function(accessToken, refreshToken, profile, done) {
